95065c63a79b2a69621767c8fd655443c59c5fe8
kjljixx <kjljixx@gmail.com>
2024-03-09 17:15:26 -0500

    Fix not using rootExplorationFactor
    Fix not using rootExplorationFactor. This was accidentally broken in the previous commit
    
    SPRT against last commit with elo0=0 elo1=10 alpha=0.05 beta=0.05 tc=6+0.06:
    Score of auroradev vs aurora: 760 - 665 - 1030  [0.519] 2455
    ...      auroradev playing White: 506 - 208 - 514  [0.621] 1228
    ...      auroradev playing Black: 254 - 457 - 516  [0.417] 1227
    ...      White vs Black: 963 - 462 - 1030  [0.602] 2455
    Elo difference: 13.5 +/- 10.5, LOS: 99.4 %, DrawRatio: 42.0 %
    SPRT: llr 2.96 (100.6%), lbound -2.94, ubound 2.94 - H1 was accepted

--- search.h
+++ search.h
@@ -151,7 +151,7 @@ Node* selectChild(Node* parent, bool isRoot){
   float maxPriority = -2;
   uint8_t maxPriorityNodeIndex = 0;
 
-  const float parentVisitsTerm = (isRoot ? Aurora::options["explorationFactor"].value : Aurora::options["explorationFactor"].value)*std::sqrt(std::log(parent->visits));
+  const float parentVisitsTerm = (isRoot ? Aurora::options["rootExplorationFactor"].value : Aurora::options["explorationFactor"].value)*std::sqrt(std::log(parent->visits));
 
   //const float parentVisitsTerm = eP[5]*powl(eP[2]*logl(eP[0]*parent->visits+eP[1])+eP[3], eP[4])+eP[6];
 
