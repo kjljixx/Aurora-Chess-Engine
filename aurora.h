9bd9210eaf420d04e3bbd31552f2a725ecd6b6df
kjljixx <kjljixx@gmail.com>
2024-07-10 18:10:12 -0400

    Update Version
    Update Version

--- aurora.h
+++ aurora.h
@@ -4,7 +4,7 @@
 //Set to 1 if you want to build a version of Aurora which generates data, 2 for generating data while playing (cutechess), 0 for the normal version.
 #define DATAGEN 0
 
-#define VERSION "v1.14.1-refactor-node"
+#define VERSION "v1.14.2-update-node-output"
 
 namespace Aurora{
 
