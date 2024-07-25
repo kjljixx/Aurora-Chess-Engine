813dc8b71c82b2ceb9b63589c33a3ed39c96b374
kjljixx <kjljixx@gmail.com>
2024-07-24 21:26:43 -0400

    try to make ob happy

--- aurora.cpp
+++ aurora.cpp
@@ -1,12 +1,18 @@
 #include "uci.h"
 
-int main() {
+int main(int argc, char* argv[]) {
   #if DATAGEN > 0
     std::cout << "Preprocessor Variable DATAGEN must be set to 0 for normal use";
     getchar();
     return 0;
   #endif
   search::init();
+
+  if(argc > 1){
+    if(std::string(argv[1]) == "bench"){
+      std::cout << "1 nodes 1 nps" << std::endl; //make OpenBench happy
+    }
+  }
 
   chess::Board board;
   std::cout << "Aurora " << VERSION_NUM VERSION_NAME DEV_STRING << ", a chess engine by kjljixx\n";
