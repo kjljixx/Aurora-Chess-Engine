bf246050a1e212c492785aba6c488a5b50db2031
kjljixx <kjljixx@gmail.com>
2024-07-25 09:32:48 -0400

    Make Aurora compatible with OpenBench
    Aurora now has an OB instnace! https://kjljixx.pythonanywhere.com/

--- aurora.h
+++ aurora.h
@@ -32,6 +32,7 @@ std::map<std::string, Option> options;
 
 void initOptions(){
   options["Hash"] = Option(0, 0, 65536, 1);
+  options["Threads"] = Option(1, 1, 1, 1); //just here to make OpenBench happy
 
   options["outputLevel"] = Option(2, 0, 3, 1);
   //0: only output bestmove at end of search
