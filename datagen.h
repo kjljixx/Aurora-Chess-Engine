3d9fd7c4c5a82e1d911e9cc84f1b156a9b5c9943
kjljixx <kjljixx@gmail.com>
2024-07-29 20:55:26 -0400

    Revert "this should not improve"
    This reverts commit 443cf460d8a87ba787c4cd7de1ac40b9c166043f.

--- datagen.h
+++ datagen.h
@@ -128,8 +128,7 @@ int main(){
 
         search::makeMove(board, chosenEdge.edge, rootBoard, tree);
 
-        if((chess::getGameStatus(board, chess::isLegalMoves(board)) != chess::ONGOING) || std::abs(rootVal)>0.9999 || chess::probeWdlTb(board) != chess::ONGOING){
-          assert(std::abs(rootVal) >= 0.9999 || rootVal == 0);
+        if((chess::getGameStatus(board, chess::isLegalMoves(board)) != chess::ONGOING) || std::abs(rootVal)>0.9999){
           gameIter++;
           if(gameIter % infoPrintInterval == 0){
             std::cout << "Thread: " << threadId << "\n";
