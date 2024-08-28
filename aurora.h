4262f37eda4e4025dca99af0107cf8d89566e8e1
kjljixx <kjljixx@gmail.com>
2024-08-28 19:10:51 -0400

    Update Version
    Update Version

--- aurora.h
+++ aurora.h
@@ -5,8 +5,8 @@
 #define DATAGEN 0
 
 
-#define VERSION_NUM "v1.18.1"
-#define VERSION_NAME "-fix-depth"
+#define VERSION_NUM "v1.18.2"
+#define VERSION_NAME "-total-time"
 #ifdef DEV
 #define DEV_STRING "-dev"
 #else
