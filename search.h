69067644c3624516c078229e18b4f5e1692f08c3
kjljixx <kjljixx@gmail.com>
2025-01-18 17:13:11 -0500

    Update search.h

--- search.h
+++ search.h
@@ -491,7 +491,7 @@ void backpropagate(Tree& tree, float result, std::vector<std::pair<Edge*, U64>>&
         continueBackprop = false;
 
         currEdge->child->iters++;
-        float newValWeight = fminf(1.0, fmaxf(valSameMinWeight, 1.0/currEdge->child->iters));
+        float newValWeight = fminf(1.0, fmaxf(valSameMinWeight, 1.0/std::sqrt(currEdge->child->iters)));
         currEdge->child->avgValue = currEdge->child->avgValue*(1-newValWeight) + currEdge->value*newValWeight;
 
         TTEntry* entry = tree.getTTEntry(hash);
@@ -514,12 +514,12 @@ void backpropagate(Tree& tree, float result, std::vector<std::pair<Edge*, U64>>&
       result = -currEdge->value;
 
       currEdge->child->iters++;
-      float newValWeight = fminf(1.0, fmaxf(valChangedMinWeight, 1.0/currEdge->child->iters));
+      float newValWeight = fminf(1.0, fmaxf(valChangedMinWeight, 1.0/std::sqrt(currEdge->child->iters)));
       currEdge->child->avgValue = currEdge->child->avgValue*(1-newValWeight) + currEdge->value*newValWeight;
     }
     else{
       currEdge->child->iters++;
-      float newValWeight = fminf(1.0, fmaxf(valSameMinWeight, 1.0/currEdge->child->iters));
+      float newValWeight = fminf(1.0, fmaxf(valSameMinWeight, 1.0/std::sqrt(currEdge->child->iters)));
       currEdge->child->avgValue = currEdge->child->avgValue*(1-newValWeight) + currEdge->value*newValWeight;
     }
   }
