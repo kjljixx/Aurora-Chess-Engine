18cc18b6a41eb248e1829eb4de8c3ec2264db477
kjljixx <kjljixx@gmail.com>
2025-01-19 08:33:23 -0500

    Update TT from all qSearch results
    Update TT from all qSearch results
    
    Elo   | 34.63 +- 17.01 (95%)
    SPRT  | 6.0+0.06s Threads=1 Hash=1MB TTHash=16MB
    LLR   | 3.05 (-2.94, 2.94) [0.00, 10.00]
    Games | N: 916 W: 354 L: 263 D: 299
    Penta | [32, 71, 178, 128, 49]
    https://kjljixx.pythonanywhere.com/test/178/
    
    Elo   | 52.90 +- 21.46 (95%)
    SPRT  | 20.0+0.20s Threads=1 Hash=4MB TTHash=16MB
    LLR   | 2.97 (-2.94, 2.94) [0.00, 10.00]
    Games | N: 556 W: 217 L: 133 D: 206
    Penta | [13, 40, 109, 82, 34]
    https://kjljixx.pythonanywhere.com/test/179/

--- aurora.h
+++ aurora.h
@@ -5,8 +5,8 @@
 #define DATAGEN 0
 
 
-#define VERSION_NUM "v1.25.1"
-#define VERSION_NAME "-smaller-tt-entry"
+#define VERSION_NUM "v1.25.2"
+#define VERSION_NAME "-save-tt-entry"
 #ifdef DEV
 #define DEV_STRING "-dev"
 #else
