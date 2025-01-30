9c2d0d6fe41c89a9fc73dbe88bac9212ae57db6c
kjljixx <kjljixx@gmail.com>
2025-01-30 06:58:35 -0500

    bench 526866

--- search.h
+++ search.h
@@ -366,13 +366,17 @@ uint8_t selectEdge(Node* parent, bool isRoot){
 
   double sumDiscountedVisits = 0;
   for(int i=0; i<parent->children.size(); i++){
-    if(!parent->children[i].child) continue;
-    sumDiscountedVisits += parent->children[i].child->discountedVisits;
+    if(!parent->children[i].child){
+      sumDiscountedVisits += 1.0;
+    }
+    else{
+      sumDiscountedVisits += parent->children[i].child->discountedVisits;
+    }
   }
   
   float parentVisitsTerm = 0;
   if(isRoot){
-    parentVisitsTerm = 0.15*std::sqrt(std::log(std::max(sumDiscountedVisits, 2.0)));
+    parentVisitsTerm = Aurora::rootExplorationFactor.value*std::log(sumDiscountedVisits)*std::sqrt(std::log(sumDiscountedVisits));
   }
   else{
     parentVisitsTerm = Aurora::explorationFactor.value*std::log(parent->visits)*std::sqrt(std::log(parent->visits));
@@ -397,7 +401,7 @@ uint8_t selectEdge(Node* parent, bool isRoot){
       currPriority = -(currNode ? currNode->avgValue : currEdge.value)+
         (parent->visits*0.0004 > (currNode ? currNode->visits : 1) ? 2 : 1)*
         varianceScale*
-        parentVisitsTerm/std::sqrt(std::max(currNode ? currNode->discountedVisits : (isLRUPruned ? 14 : 0.001), 0.001));
+        parentVisitsTerm/std::sqrt(std::max(currNode ? currNode->discountedVisits : (isLRUPruned ? 14 : 1), 1.0));
     }
     else{
       currPriority = -(currNode ? currNode->avgValue : currEdge.value)+
@@ -470,7 +474,7 @@ void backpropagate(Tree& tree, float result, std::vector<std::pair<Edge*, U64>>&
   Node* parent = currEdge->child->parent;
   for(int i=0; i<parent->children.size(); i++){
     if(parent->children[i].child){
-      parent->children[i].child->discountedVisits *= 0.99999;
+      parent->children[i].child->discountedVisits *= 0.9999;
     }
   }
   currEdge->child->discountedVisits += visits;
