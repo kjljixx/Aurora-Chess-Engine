79345348dd94660531c60b416ed269a32002857c
kjljixx <kjljixx@gmail.com>
2025-02-01 18:50:36 -0500

    bench 530388

--- search.h
+++ search.h
@@ -378,10 +378,10 @@ uint8_t selectEdge(Node* parent, bool isRoot){
   
   float parentVisitsTerm = 0;
   if(isRoot){
-    parentVisitsTerm = Aurora::rootExplorationFactor.value*std::log(sumDiscountedVisits);
+    parentVisitsTerm = Aurora::rootExplorationFactor.value*std::sqrt(std::log(sumDiscountedVisits))*std::log(sumDiscountedVisits);
   }
   else{
-    parentVisitsTerm = Aurora::explorationFactor.value*std::log(sumDiscountedVisits);
+    parentVisitsTerm = Aurora::explorationFactor.value*std::sqrt(std::log(sumDiscountedVisits))*std::log(sumDiscountedVisits);
   }
 
   float varianceScale = 
