5be0f29dfa82fdacd1d065e7a3a37162c6fdd870
kjljixx <kjljixx@gmail.com>
2026-04-15 21:17:09 -0400

    bench 506749

--- search.h
+++ search.h
@@ -763,7 +763,7 @@ inline void search(chess::Board& rootBoard, timeManagement tm, Tree& tree){
           float seeValue = evaluation::SEE(movedBoard, currEdge->edge.getEndSquare(), -10000000, currEdge->edge.getStartSquare());
           // movedBoard.printBoard();
           // std::cout << currEdge->edge.toStringRep() << "\n";
-          if(seeValue < 0){
+          if(seeValue < -100){
             // std::cout << "\n";
             // std::cout << seeValue << "\n";
             float currCpValue = evaluation::valToCp(parentNode->avgValue);
