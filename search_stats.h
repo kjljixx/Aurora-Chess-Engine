c13f5baa0f5afd7c99facd3d704fa03d29169397
kjljixx <kjljixx@gmail.com>
2026-08-05 23:33:18 -0400

    Add search_stats.h I'm dumb bench 467874

---
+++ search_stats.h
@@ -0,0 +1,167 @@
+#pragma once
+#include <cstdint>
+#include <iostream>
+
+#ifdef DEV
+
+namespace search{
+
+enum class StopReason : uint8_t{
+  None = 0,
+  Soft,
+  Hard,
+  Nodes,
+  Iters,
+  Forever,
+  Time
+};
+
+inline const char* stopReasonName(StopReason r){
+  switch(r){
+    case StopReason::Soft: return "soft";
+    case StopReason::Hard: return "hard";
+    case StopReason::Nodes: return "nodes";
+    case StopReason::Iters: return "iters";
+    case StopReason::Forever: return "forever";
+    case StopReason::Time: return "time";
+    default: return "none";
+  }
+}
+
+struct SearchStats{
+  uint64_t playoutTerminal = 0;
+  uint64_t playoutTb = 0;
+  uint64_t playoutTtHit = 0;
+  uint64_t playoutEval = 0;
+
+  uint64_t ttStores = 0;
+  uint64_t ttCollisions = 0;
+
+  uint64_t lruEvictions = 0;
+  uint64_t lruPrunedEdgeSelects = 0;
+
+  uint64_t backpropValueChanged = 0;
+  uint64_t backpropVisitOnly = 0;
+
+  uint64_t rootQADisagree = 0;
+  uint64_t rootQADisagreeChecks = 0;
+
+  uint64_t visitWindowVisitsSum = 0;
+  uint64_t visitWindowIters = 0;
+  uint64_t tmBestMoveChanges = 0;
+  float tmSoftMultiplier = 1.0f;
+
+  StopReason stopReason = StopReason::None;
+  uint64_t treeReuseNodesKept = 0;
+  uint64_t treeReuseEvents = 0;
+  uint64_t destroyTreeEvents = 0;
+
+  uint64_t expandBranchingSum = 0;
+  uint64_t expandBranchingCount = 0;
+  uint64_t effectiveBranchingSum = 0;
+  uint64_t effectiveBranchingCount = 0;
+
+  uint64_t qsearchNodes = 0;
+  uint64_t qsearchDeltaPrune = 0;
+  uint64_t qsearchSeePrune = 0;
+  uint64_t qsearchBetaCut = 0;
+  uint64_t qsearchMaxPly = 0;
+  uint64_t pathDepth0_4 = 0;
+  uint64_t pathDepth5_8 = 0;
+  uint64_t pathDepth9_16 = 0;
+  uint64_t pathDepth17Plus = 0;
+
+  void reset(){
+    *this = SearchStats{};
+  }
+
+  void recordPathDepth(int depth){
+    if(depth <= 4) pathDepth0_4++;
+    else if(depth <= 8) pathDepth5_8++;
+    else if(depth <= 16) pathDepth9_16++;
+    else pathDepth17Plus++;
+  }
+
+  void recordTtStore(uint32_t existingHash, float existingVal, uint32_t newHashHi){
+    if(existingVal != -2 && existingHash != newHashHi){
+      ttCollisions++;
+    }
+    ttStores++;
+  }
+
+  void print() const{
+    const uint64_t playoutTotal = playoutTerminal + playoutTb + playoutTtHit + playoutEval;
+    const double visitWindowMean = visitWindowIters
+      ? double(visitWindowVisitsSum) / double(visitWindowIters)
+      : 0.0;
+    const double expandBranchingMean = expandBranchingCount
+      ? double(expandBranchingSum) / double(expandBranchingCount)
+      : 0.0;
+    const double effectiveBranchingMean = effectiveBranchingCount
+      ? double(effectiveBranchingSum) / double(effectiveBranchingCount)
+      : 0.0;
+
+    std::cout << "info string searchstats playout"
+              << " terminal=" << playoutTerminal
+              << " tb=" << playoutTb
+              << " ttHit=" << playoutTtHit
+              << " eval=" << playoutEval
+              << " total=" << playoutTotal << "\n";
+    std::cout << "info string searchstats tt"
+              << " stores=" << ttStores
+              << " collisions=" << ttCollisions << "\n";
+    std::cout << "info string searchstats lru"
+              << " evictions=" << lruEvictions
+              << " prunedEdgeSelects=" << lruPrunedEdgeSelects << "\n";
+    std::cout << "info string searchstats backprop"
+              << " valueChanged=" << backpropValueChanged
+              << " visitOnly=" << backpropVisitOnly << "\n";
+    std::cout << "info string searchstats root"
+              << " qaDisagree=" << rootQADisagree
+              << " qaChecks=" << rootQADisagreeChecks << "\n";
+    std::cout << "info string searchstats branching"
+              << " expandMean=" << expandBranchingMean
+              << " expandSamples=" << expandBranchingCount
+              << " effectiveMean=" << effectiveBranchingMean
+              << " effectiveSamples=" << effectiveBranchingCount << "\n";
+    std::cout << "info string searchstats visitWindow"
+              << " visitsSum=" << visitWindowVisitsSum
+              << " iters=" << visitWindowIters
+              << " meanVisits=" << visitWindowMean << "\n";
+    std::cout << "info string searchstats tm"
+              << " bestMoveChanges=" << tmBestMoveChanges
+              << " softMultiplier=" << tmSoftMultiplier
+              << " stopReason=" << stopReasonName(stopReason) << "\n";
+    std::cout << "info string searchstats reuse"
+              << " nodesKept=" << treeReuseNodesKept
+              << " reuseEvents=" << treeReuseEvents
+              << " destroyEvents=" << destroyTreeEvents << "\n";
+    std::cout << "info string searchstats qsearch"
+              << " nodes=" << qsearchNodes
+              << " deltaPrune=" << qsearchDeltaPrune
+              << " seePrune=" << qsearchSeePrune
+              << " betaCut=" << qsearchBetaCut
+              << " maxPly=" << qsearchMaxPly << "\n";
+    std::cout << "info string searchstats pathDepth"
+              << " 0-4=" << pathDepth0_4
+              << " 5-8=" << pathDepth5_8
+              << " 9-16=" << pathDepth9_16
+              << " 17+=" << pathDepth17Plus << "\n";
+  }
+};
+
+inline thread_local SearchStats* g_searchStats = nullptr;
+
+} // namespace search
+
+#define SEARCH_STAT(field) do { if (::search::g_searchStats) ++(::search::g_searchStats->field); } while(0)
+#define SEARCH_STAT_ADD(field, n) do { if (::search::g_searchStats) (::search::g_searchStats->field) += (n); } while(0)
+#define SEARCH_STAT_MAX(field, n) do { if (::search::g_searchStats && (n) > ::search::g_searchStats->field) ::search::g_searchStats->field = (n); } while(0)
+
+#else // !DEV
+
+#define SEARCH_STAT(field) ((void)0)
+#define SEARCH_STAT_ADD(field, n) ((void)0)
+#define SEARCH_STAT_MAX(field, n) ((void)0)
+
+#endif // DEV
